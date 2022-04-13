287a97f5e9a5ced862b0f78ee0b371626986b04b
Victor Pereira <victordiaspereira250@gmail.com>
2022-04-13 19:15:01 -0300

    add requiriments

--- api_todo/settings.py
+++ api_todo/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'api_todo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR/'db.sqlite3',
     }
 }
 
